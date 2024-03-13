c172c6339c03c345ce69eb9d48f381b017339426
Darkrai9x <longvu.uet@gmail.com>
2024-03-13 16:01:30 +0700

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
   id("java")
-  id("maven-publish")
 }
 
 group = "io.github.Darkrai9x"
@@ -8,18 +7,4 @@ version = "0.12.1"
 
 repositories {
   mavenCentral()
-}
-
-publishing {
-  publications {
-    withType<MavenPublication> {
-      pom {
-        url.set("https://github.com/jonelo/jAdapterForNativeTTS")
-        name.set("jadapter-for-native-tts")
-        scm {
-          url.set("https://github.com/jonelo/jAdapterForNativeTTS")
-        }
-      }
-    }
-  }
 }
